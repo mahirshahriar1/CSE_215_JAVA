32388faab2323243d8af26a65061da61b3d2ed75
Mahir Shahriar Tamim <95174683+mahirshahriar1@users.noreply.github.com>
2022-07-22 20:25:29 +0600

    Create Point.java

---
+++ OOP/Basic OOP/Point.java
@@ -0,0 +1,43 @@
+//Point and lines are connected
+
+package folder1;
+
+public class Point {	
+	private int x;
+	private int y;
+	
+	public Point() {
+		x=0;
+		y=0;
+	}
+	
+	public Point(int x, int y) 
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() 
+	{
+		return x;
+	}
+	
+	public void setX(int x) 
+	{
+		this.x = x;
+	}
+	
+	public int getY() 
+	{
+		return y;
+	}
+	
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+	
+	public String toString() {
+		return "Point[x: " + x + ", y: " + y + "]";
+	}
+}
