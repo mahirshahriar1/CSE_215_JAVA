5397647ffdf95c91de2a965212e15f47065ffca5
Mahir Shahriar Tamim <95174683+mahirshahriar1@users.noreply.github.com>
2022-08-14 19:32:06 +0600

    Update Ser.java

--- File/Serializable/Ser.java
+++ File/Serializable/Ser.java
@@ -50,3 +50,46 @@ public class Ser {
 	}
 
 }
+
+/*public class Ser {
+
+	public static void main(String[] args) {
+		File file = new File("Employee.ser");
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+
+		try {
+			Scanner scan = new Scanner(System.in);
+
+			fos = new FileOutputStream(file);
+			oos = new ObjectOutputStream(fos);
+
+			Employee[] emp = new Employee[3];
+
+			for (int i = 0; i < 3; i++) {
+				System.out.print("Enter Name of Employee " + (i + 1) + ": ");
+				String name = scan.nextLine();
+				System.out.print("Enter ID of Employee " + (i + 1) + ": ");
+				int id = scan.nextInt();
+				emp[i] = new Employee(name, id);
+				scan.nextLine();
+			}
+			scan.close();
+			oos.writeObject(emp);
+			System.out.println("Serialization done successfully!!");
+			System.out.println();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fos.close();
+				oos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
